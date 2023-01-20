6707aa5da63a430170a492ef6d907df0999024ad
amansoni <amansoni2041999@gmail.com>
2023-01-20 17:04:46 +0530

    performing Json program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+
+{
+    internal class JsonInventoryMain
+
+    {
+        static void Main(string[] args)
+        {
+            JsonInventoryMain Json = new JsonInventoryMain();
+            Json.Convert("D:\\Batch 244\\Json_Demo\\Inventory.json");
+        }
+    }
+}
